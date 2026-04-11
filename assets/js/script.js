74ef5f466af9368312623de75c8d478a57c138ef
MOudelha <moumouhoud2008@gmail.com>
2026-04-11 21:13:27 +0200

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,4 +0,0 @@
-
-const toggle=document.querySelector('[data-nav-toggle]');
-const nav=document.querySelector('[data-nav]');
-if(toggle&&nav){toggle.addEventListener('click',()=>{const o=nav.classList.toggle('open');toggle.setAttribute('aria-expanded',o?'true':'false')});nav.querySelectorAll('a').forEach(a=>a.addEventListener('click',()=>{nav.classList.remove('open');toggle.setAttribute('aria-expanded','false')}));}
